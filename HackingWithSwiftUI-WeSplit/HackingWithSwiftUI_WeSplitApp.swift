4779adbbd3a0479928a334f03e2f665132073c04
Michael Jones <michael.jones90@me.com>
2026-05-24 08:17:44 +0100

    Initial Commit

---
+++ HackingWithSwiftUI-WeSplit/HackingWithSwiftUI_WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackingWithSwiftUI_WeSplitApp.swift
+//  HackingWithSwiftUI-WeSplit
+//
+//  Created by Michael Jones on 24/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct HackingWithSwiftUI_WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
